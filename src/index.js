f823cdbd543316cf6b82d58cb7d345e289201656
Joao Bosco <jboscos.carvalho@gmail.com>
2022-11-21 08:42:34 -0300

    Implementação de Sign Up e Sign In

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import joi from "joi";
 import { MongoClient, MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
 dotenv.config();
 
 const app = express();
@@ -19,6 +21,34 @@ try {
   console.log(err);
 }
 
-app.post("/", async (req, res) => {});
+const db = mongoClient.db("myWallet");
+const usersCollection = db.collection("users");
+const sessionsCollection = db.collection("sessions");
+
+app.post("/signUp", async (req, res) => {
+  const user = req.body;
+  const passwordHash = bcrypt.hashSync(user.password, 10);
+
+  await db.collection("users").insertOne({ ...user, password: passwordHash });
+  res.sendStatus(201);
+});
+
+app.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  const token = uuidV4();
+
+  const user = await usersCollection.findOne({ email });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
+    await sessionsCollection.insertOne({
+      token,
+      userId: user._id,
+    });
+
+    res.send({ token });
+  } else {
+    res.sendStatus(401);
+  }
+});
 
 app.listen(5000, () => console.log("Port 5000"));
